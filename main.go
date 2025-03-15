4b4a7ad8419e9f6a8f730a2331f1a8a0b6e14dc3
Md Nazmul Hasan <farhapartex@gmail.com>
2025-03-16 00:51:04 +0600

    (feat) added basic gin setup

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Ghor BE")
+	r := gin.Default()
+
+	r.GET("/", HealthCheckHandler)
+	r.GET("/health_check", HealthCheckHandler)
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal("Error from main: ", err)
+	}
+
+	fmt.Println("server running on port 8080")
+}
+
+func HealthCheckHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "System is up and running",
+	})
 }
