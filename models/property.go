fb2b10309597c71b361bfd8ecf00f8973833fa26
Md Nazmul Hasan <farhapartex@gmail.com>
2025-04-18 16:54:51 +0600

    (feat) added model for Property feature

--- models/property.go
+++ models/property.go
@@ -49,3 +49,96 @@ type Property struct {
 	ApprovedByID *uint      `json:"approved_by_id"`
 	ApprovedBy   *User      `gorm:"foreignKey:ApprovedByID" json:"approved_by"`
 }
+
+type Amenities struct {
+	AirConditioning  bool    `json:"airConditioning"`
+	Heating          bool    `json:"heating"`
+	Furnished        bool    `json:"furnished"`
+	PetsAllowed      bool    `json:"petsAllowed"`
+	WasherDryer      bool    `json:"washerDryer"`
+	InUnitLaundry    bool    `json:"inUnitLaundry"`
+	Elevator         bool    `json:"elevator"`
+	OutdoorSpace     bool    `json:"outdoorSpace"`
+	Balcony          bool    `json:"balcony"`
+	DisabilityAccess bool    `json:"disabilityAccess"`
+	HardwoodFloors   bool    `json:"hardwoodFloors"`
+	Fireplace        bool    `json:"fireplace"`
+	Pool             bool    `json:"pool"`
+	Gym              bool    `json:"gym"`
+	Parking          int     `json:"parking"`
+	Garages          int     `json:"garages"`
+	CeilingHeight    float64 `json:"ceilingHeight"`
+	LotSize          float64 `json:"lotSize"`
+}
+
+type SecurityFeature struct {
+	SecuritySystem bool `json:"securitySystem"`
+	Doorman        bool `json:"doorman"`
+	SecurityCamera bool `json:"securityCamera"`
+	GatedCommunity bool `json:"gatedCommunity"`
+	FireAlarm      bool `json:"fireAlarm"`
+}
+
+type TechnologyFeature struct {
+	InternetWifi    bool `json:"internetWifi"`
+	SmartHome       bool `json:"smartHome"`
+	Dishwasher      bool `json:"dishwasher"`
+	GarbageDisposal bool `json:"garbageDisposal"`
+	CableTV         bool `json:"cableTV"`
+	Refrigerator    bool `json:"refrigerator"`
+	Microwave       bool `json:"microwave"`
+	StoveOven       bool `json:"stoveOven"`
+	CeilingFans     bool `json:"ceilingFans"`
+}
+
+type LuxuryFeature struct {
+	Fireplace     bool `json:"fireplace"`
+	Pool          bool `json:"pool"`
+	Gym           bool `json:"gym"`
+	WalkInClosets bool `json:"walkInClosets"`
+	Jacuzzi       bool `json:"jacuzzi"`
+	Sauna         bool `json:"sauna"`
+}
+
+type CommunityFeature struct {
+	Concierge       bool `json:"concierge"`
+	BusinessCenter  bool `json:"businessCenter"`
+	ConferenceRoom  bool `json:"conferenceRoom"`
+	GuestParking    bool `json:"guestParking"`
+	Playground      bool `json:"playground"`
+	BBQArea         bool `json:"bbqArea"`
+	CommunityGarden bool `json:"communityGarden"`
+	TennisCourt     bool `json:"tennisCourt"`
+	BasketballCourt bool `json:"basketballCourt"`
+}
+
+type UtilsFeature struct {
+	WaterIncluded        bool `json:"waterIncluded"`
+	GasIncluded          bool `json:"gasIncluded"`
+	ElectricityIncluded  bool `json:"electricityIncluded"`
+	TrashRemovalIncluded bool `json:"trashRemovalIncluded"`
+	InternetIncluded     bool `json:"internetIncluded"`
+}
+
+type EnergyFeature struct {
+	SolarPanels               bool `json:"solarPanels"`
+	EnergyEfficientAppliances bool `json:"energyEfficientAppliances"`
+	GreenCertification        bool `json:"greenCertification"`
+	EVCharging                bool `json:"evCharging"`
+	RainwaterHarvesting       bool `json:"rainwaterHarvesting"`
+	ProgrammableThermostat    bool `json:"programmableThermostat"`
+}
+
+type PropertyFeature struct {
+	gorm.Model
+	PropertyID        uint              `json:"property_id"`
+	Property          Property          `gorm:"foreignKey:PropertyID" json:"property"`
+	Features          []string          `gorm:"type:text[]" json:"features"`
+	Amenities         Amenities         `gorm:"type:jsonb" json:"amenities"`
+	SecurityFeature   SecurityFeature   `gorm:"type:jsonb" json:"securityFeature"`
+	TechnologyFeature TechnologyFeature `gorm:"type:jsonb" json:"technologyFeature"`
+	LuxuryFeature     LuxuryFeature     `gorm:"type:jsonb" json:"luxuryFeature"`
+	CommunityFeature  CommunityFeature  `gorm:"type:jsonb" json:"communityFeature"`
+	UtilsFeature      UtilsFeature      `gorm:"type:jsonb" json:"utilsFeature"`
+	EnergyFeature     EnergyFeature     `gorm:"type:jsonb" json:"energyFeature"`
+}
